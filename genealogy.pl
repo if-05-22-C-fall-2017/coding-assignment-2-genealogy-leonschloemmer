2df2a6b517a13736a467221f51dac976d33cc702
leonschloemmer <leon.schloemmer@gmail.com>
2017-11-29 16:34:29 +0100

    Worked on genealogy.pl.

---
+++ genealogy.pl
@@ -0,0 +1,24 @@
+parent/2.
+
+parent(adam, john).
+parent(eve, john).
+parent(eve, lisa).
+parent(john, anne).
+parent(john, pat).
+parent(pat, jacob).
+parent(carol, jacob).
+
+male/1.
+female/1.
+
+male(adam).
+male(john).
+male(pat).
+male(jacob).
+female(eve).
+female(anne).
+female(lisa).
+female(carol).
+
+grandparent/2.
+grandparent(GP, GC) :- parent(C, GC), parent(GP, C).
